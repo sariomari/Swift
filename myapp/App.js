1abf1952c2ecb4ef5729a0c6183539eb9552c1c4
sariomari <73112567+sariomari@users.noreply.github.com>
2023-05-12 17:52:57 +0300

    Made the login screen a separate component/screen, the app imports it from screens/ folder

--- myapp/App.js
+++ myapp/App.js
@@ -1,88 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from 'react-native';
+import { StyleSheet, SafeAreaView} from 'react-native';
+import LoginScrn from './screens/loginScreen';
 
 export default function App() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
   return (
-    <View style={styles.container}>
-      <View style={styles.logo}>
-        <Image style={styles.image} source = {require("./assets/logo.jpeg")}/>
-      </View>
-      <View style={styles.inputView}>
-        <TextInput
-          style={styles.TextInput}
-          placeholder="Email"
-          placeholderTextColor="#003f5c"
-          onChangeText={(email) => setEmail(email)}
-        />
-      </View>
-      <View style={styles.inputView}>
-        <TextInput
-          style={styles.TextInput}
-          placeholder="Password"
-          placeholderTextColor="#003f5c"
-          secureTextEntry={true}
-          onChangeText={(password) => setPassword(password)}
-        />
-      </View>
-      <TouchableOpacity>
-        <Text style={styles.normalText}>Forgot Password?</Text> 
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.loginBtn}>
-        <Text style={styles.loginText}>LOGIN</Text> 
-      </TouchableOpacity>
-      <TouchableOpacity>
-        <Text style={styles.normalText}>Don't have an account?</Text> 
-      </TouchableOpacity>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style = {styles.root}>
+      <LoginScrn />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  root: {
     flex: 1,
-    backgroundColor: "#000000",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  logo: {
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  loginBtn: {
-    width: "80%",
-    borderRadius: 25,
-    height: 50,
-    alignItems: "center",
-    justifyContent: "center",
-    marginTop: 40,
-    backgroundColor: "#fff",
-  },
-  inputView: {
-    backgroundColor: "#fff",
-    borderRadius: 50,
-    width: "70%",
-    height: 45,
-    marginBottom: 20,
-    alignItems: "center",
-  },
-  TextInput: {
-    height: 50,
-    flex: 1,
-    padding: 10,
-    marginLeft: 20,
-  },
-  image: {
-    width: 200,
-    height: 200,
-  },
-  normalText: {
-    height: 30,
-    marginBottom: 30,
-    color: "white",
-    padding: 10,
-  },
+  }
 });
